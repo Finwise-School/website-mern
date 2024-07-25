41eda35178cc0f0d3accf35991c72e4f8e86a7af
AMANDEEP SINGH BHALLA <amandeepsinghbhalla.ab@outlook.com>
2024-07-25 22:11:17 +0530

    Add files via upload

---
+++ clients-page/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Cards from "./components/carouselCards";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+
+function App() {
+  return (
+    <>
+    <Cards/>
+    </>
+  );
+}
+
+export default App;
