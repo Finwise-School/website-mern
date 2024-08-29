6540ec74943c1e8bd8b90d519aa5c5594f0f721b
Keshav Gupta <guptakeshav0710@gmail.com>
2024-08-30 01:47:48 +0530

    header and footer

---
+++ frontend/src/components/Homepage/Header Files/RequestEarlyAccess.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function RequestAccessButton() {
+  return (
+    <button className="gap-2 self-stretch pt-4 pb-5 pl-5 my-auto font-extrabold text-blue-900 bg-white rounded-xl max-md:px-5">
+      Request Early Access
+    </button>
+  );
+}
+
+export default RequestAccessButton;
