b5b42b5bd92e3d2f08201fc6ad70d4866167caf1
AMANDEEPSINGHBHALLA <singhbhallaa@gmail.com>
2024-09-02 20:40:51 +0530

    Udated Blogs & FAQ

--- frontend/src/components/Blogs/cardsBottom.js
+++ frontend/src/components/Blogs/cardsBottom.js
@@ -3,23 +3,25 @@ import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import axios from 'axios';
 import Wallimg from '../../assets/images/Blogs/img2.png';
+import Thumbnail from '../../assets/images/Blogs/b-2.png'
 import Buttonnext from "../../assets/images/Blogs/Buttonnext.png";
 import Buttonprev from "../../assets/images/Blogs/Buttonprev.png";
 
 const CardsBottom = ({ onToggle }) => {
 
-  const [data, setData] = useState([]);
+  // const [data, setData] = useState([]); DONT DELETE
   const carouselRef = useRef(null);
 
-  useEffect(() => {
-    axios.get('http://localhost:2142/api')
-    .then(response => {
-      // setData(response.data[0].Heading);
-      setData(response.data);
-      console.log("Hello");
-    })
-    .catch(error => console.error('Error fetching data:', error));
-  }, []);
+  // DONT DELETE
+  // useEffect(() => {
+  //   axios.get('http://localhost:2142/api')
+  //   .then(response => {
+  //     // setData(response.data[0].Heading);
+  //     setData(response.data);
+  //     console.log("Hello");
+  //   })
+  //   .catch(error => console.error('Error fetching data:', error));
+  // }, []);
 
 const responsive = {
   superLargeDesktop: {
@@ -45,22 +47,20 @@ const responsive = {
 };
 return (
   <>
-<Carousel ref={carouselRef} partialVisible={true} responsive={responsive}>
-{data.map((item, index) => {
-        return (
-          <div key={item._id || index} className='cursor-pointer m-4  bg-slate-100 rounded-xl' onClick={() => onToggle(item)}>
-            <div className="rounded-t flex justify-center flex-wrap overflow-hidden">
-              <img src={Wallimg} alt="" />
-            </div>
-            <div className="content py-2">
-              <div className="flex flex-col justify-start">
-                <h3 className="text-sm font-bold">{item.Heading.slice(0, 35)}</h3>
-                <h6 className="text-sm">Date</h6>
-              </div>
-            </div>
-          </div>
-        );
-      })}
+<Carousel ref={carouselRef} responsive={responsive}>
+{Array.from({ length: 5 }, (_, index) => (
+    <div key={index} className='cursor-pointer m-4  bg-slate-100 rounded-xl'>
+      <div className="rounded-t flex justify-center flex-wrap overflow-hidden">
+        <img src={Thumbnail} alt="" />
+      </div>
+      <div className="content py-2">
+        <div className="flex flex-col justify-start">
+          <h3 className="text-sm font-bold">The UK Tax System</h3>
+          <h6 className="text-sm">Aug 9</h6>
+        </div>
+      </div>
+    </div>
+))}
 </Carousel>
       <div className='msFooter flex flex-row justify-center items-center my-3'>
                 <button className='custom-arrow custom-arrow-prev' onClick={() => carouselRef.current && carouselRef.current.previous()}>
